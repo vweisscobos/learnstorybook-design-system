71c43bf7175486d19151933d7022b7ec3dc52759
vweisscobos <v.weisscobos@gmail.com>
2020-03-03 09:24:58 -0300

    changing primary color

--- src/shared/styles.js
+++ src/shared/styles.js
@@ -11,7 +11,7 @@ export const background = {
 
 export const color = {
   // Palette
-  primary: '#FF4785', // coral
+  primary: '#FF47FF', // coral
   secondary: '#FFA7FD', // ocean
   tertiary: '#DDDDDD',
 
